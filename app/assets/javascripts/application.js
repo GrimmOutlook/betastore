cc60ae1ceb09f0cc9ac02b55999d08e062371290
GrimmOutlook <davegrimm2002@yahoo.com>
2013-11-07 17:01:45 -0500

    Added/Fixed AJAX to Cart, Found Syntax Error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,29 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap
-//= require turbolinks
 //= require_tree .
 
+
+
 $(document).ready(function(){
+
+console.log('In document ready')
+
+
+
+   $('a.cart').click(function() {
+     var $cart = $('#cart');
+     if($cart.is(':visible')) {
+     	$cart.hide();
+     } else {
+
+     $.get('/cart', function(html) {
+   	   $cart.html(html).show();
+     });
+     }
+     return false;
+   });
+
 	console.log('Applying click handlers to .add_to_cart');
   $('.add_to_cart').click(function(){
   	console.log('Clicked on '+this);
@@ -26,15 +45,46 @@ $(document).ready(function(){
         type: "POST",
         url: action,
         dataType: 'json',
+
+
+
         success: function(json) {
         	console.log('Response from Server');
         	console.log('json');
           $('#cart_item_count').text(json.item_count);
+
+       
+     var $cart = $('#cart');
+     if($cart.is(':visible')) {
+     	 $.get('/cart', function(html) {
+   	   $cart.html(html).show();
+     });
+     }
+
+
         }
       });
     } catch(e) {
       console.log(e);
     } finally {
   	return false;
+    }
   });
 });
+
+
+
+//  Original code to get cart item list on same page once cart is updated:
+
+//        $('a.cart').click(function() {
+//     var $cart = $('#cart');
+//     if($cart.is(':visible')) {
+//     	$cart.hide();
+//     } else {
+//
+//     $.get('/cart', function(html) {
+//   	   $cart.html(html).show();
+//     });
+//     }
+//     return false;
+//   });
