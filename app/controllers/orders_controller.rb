1724ce9c896682225b671948d52ee2ad166ab144
GrimmOutlook <davegrimm2002@yahoo.com>
2013-11-13 23:29:21 -0500

    New Order & Shopping Cart Session

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,31 @@
 class OrdersController < ApplicationController
-  
+  skip_before_filter :require_log_in, only: [:new, :create]
+
   def new
     @order = Order.new
+    Hash(session[:cart]).each do |pid, qty|
+      @order.line_items.build(
+        product_id: pid,
+        quantity: qty)
+    end
   end
 
 
-  def index
-	@page_title = "This is from the instance variable @page_title in the Orders Controller"
-	render layout: 'dave'
+#Copied from Paul's GHub - Nov. 12th
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path, notice: 'Your order has been placed'
+    else
+      flash.now[:alert] = @order.errors.full_messages.join(', ')
+      logger.error "errors: #{@order.errors.full_messages.join(', ')}"
+      render 'new'
+    end
   end
 
-
-
+  protected
+  def order_params
+    params.require(:order).permit(line_items_attributes: [:product_id, :quantity])
+  end
+end
 end
