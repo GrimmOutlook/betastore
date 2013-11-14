7cdc879cd1802864ef384aab02058d7181e2bebb
GrimmOutlook <davegrimm2002@yahoo.com>
2013-11-13 23:20:04 -0500

    New test for checkout form

---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionController::TestCase
+ 
+  test "display checkout form" do
+    get :new
+    assert_response :success
+    assert_select 'form'
+    assert_select 'input[type=submit]'
+  end
+
+  test "redirect to homepage with notice" do
+    post :create
+    assert_redirected_to  root_path
+    assert_equal "Your order has been placed", flash[:notice]
+
+  end
+end
+
