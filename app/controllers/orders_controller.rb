76df1095b8aad39b4cd49b3a23bf5eb9a72d7904
GrimmOutlook <davegrimm2002@yahoo.com>
2013-11-19 13:32:55 -0500

    Build credit_card for order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,13 +8,20 @@ class OrdersController < ApplicationController
         product_id: pid,
         quantity: qty)
     end
+   
+    @credit_card = CreditCard.new
+    Array(session[:order]) do |chname, cnum, expm, expy|
+      @order.build_credit_card(
+        cardholder_name: chname,
+        card_number: cnum.to_s,
+        exp_month: expm,
+        exp_year: expy)
+    end
   end
 
-
-#Copied from Paul's GHub - Nov. 12th
   def create
     @order = Order.new(order_params)
-    if @order.save
+    if @order.save && @credit_card.save
       redirect_to root_path, notice: 'Your order has been placed'
     else
       flash.now[:alert] = @order.errors.full_messages.join(', ')
@@ -26,6 +33,6 @@ class OrdersController < ApplicationController
   protected
   def order_params
     params.require(:order).permit(line_items_attributes: [:product_id, :quantity])
+    params.require(:credit_card).permit(credit_card_attributes: [:cardholder_name, :card_number, :exp_month, :exp_year])
   end
-end
 end
