05e37894caf6153372accee21d8a83691dc94185
GrimmOutlook <davegrimm2002@yahoo.com>
2013-11-19 13:30:15 -0500

    Created credit_card model

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -1,2 +1,12 @@
 class CreditCard < ActiveRecord::Base
+
+  attr_accessor :cardholder_name, :card_number, :exp_month, :exp_year
+
+	def initialize(cardholder_name, card_number, exp_month, exp_year)
+		@cardholder_name = cardholder_name
+		@card_number = card_number
+		@exp_month = exp_month
+		@exp_year = exp_year
+	end
+
 end
