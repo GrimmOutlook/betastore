f9946601c036d90e5a21d37809048ea7dda53a39
GrimmOutlook <grimmoutlook87@gmail.com>
2013-12-04 11:05:38 -0500

    Admin/Dashboard controller & assoc. files

---
+++ app/controllers/admin/dashboard_controller.rb
@@ -0,0 +1,11 @@
+class Admin::DashboardController < ApplicationController
+  def index
+  	sql = %{
+    select date(placed_at), count(*), sum(total_amount)
+    from orders
+    where placed_at > '#{30.days.ago}'
+    group by date(placed_at)
+    order by date(placed_at)}
+    @days = Order.connection.select_rows(sql)
+  end
+end
